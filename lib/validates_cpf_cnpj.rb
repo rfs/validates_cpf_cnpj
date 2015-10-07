a4390866c75d5b507162a753496398775fd40b52
Phelipe <phelipe.correa@truckpad.com.br>
2015-10-07 17:52:41 -0300

    CPF and CNPJ normalization

--- lib/validates_cpf_cnpj.rb
+++ lib/validates_cpf_cnpj.rb
@@ -11,16 +11,18 @@ module ActiveModel
         return if should_skip?(record, attr_name, value)
         if value.to_s.gsub(/[^0-9]/, '').length <= 11
           validate_only_cpf(record, attr_name, value)
+          normalize_cpf(record, attr_name, value)
         else
           validate_only_cnpj(record, attr_name, value)
+          normalize_cnpj(record, attr_name, value)
         end
       end
 
       protected
 
       def should_skip?(record, attr_name, value)
-        if ((options[:allow_nil] and value.nil?) or (options[:allow_blank] and value.blank?)) or  
-           ((options[:if] == false) or (options[:unless] == true)) or  
+        if ((options[:allow_nil] and value.nil?) or (options[:allow_blank] and value.blank?)) or
+           ((options[:if] == false) or (options[:unless] == true)) or
            ((options[:on].to_s == 'create' and not record.new_record?) or (options[:on].to_s == 'update' and record.new_record?))
           true
         end
@@ -37,6 +39,20 @@ module ActiveModel
         return if should_skip?(record, attr_name, value)
         if (not value.to_s.match(/\A\d{14}\z/) and not value.to_s.match(/\A\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}\z/)) or not Cnpj.valid?(value)
           record.errors.add(attr_name)
+        end
+      end
+
+      def normalize_cpf(record, attr_name, value)
+        if value
+          formatted = "#{value[0..2]}.#{value[3..5]}.#{value[6..8]}-#{value[9..10]}"
+          record.send("#{attr_name}=", formatted)
+        end
+      end
+
+      def normalize_cnpj(record, attr_name, value)
+        if value
+          formatted = "#{value[0..1]}.#{value[2..4]}.#{value[5..7]}/#{value[8..11]}-#{value[12..13]}"
+          record.send("#{attr_name}=", formatted)
         end
       end
     end
