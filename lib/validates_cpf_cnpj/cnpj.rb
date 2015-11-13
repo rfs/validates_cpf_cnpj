cc8cc889f509e5886e81bd10cdb160ca1cb33068
Mauricio <mauricioflaviomoraes@gmail.com>
2015-11-13 11:07:01 -0200

    Validation is non-destructive: doesn't change original values of CPF or CNPJ.

--- lib/validates_cpf_cnpj/cnpj.rb
+++ lib/validates_cpf_cnpj/cnpj.rb
@@ -1,19 +1,19 @@
 module ValidatesCpfCnpj
   module Cnpj
     def self.valid?(value)
-      value.gsub!(/[^0-9]/, '')
-      
+      value_only_digits = value.gsub(/[^0-9]/, '')
+
       fourteen_plus_equal_characters_regexp = /\A(\d)\1{13,}\z/
-      return false if value =~ fourteen_plus_equal_characters_regexp
-      
+      return false if value_only_digits =~ fourteen_plus_equal_characters_regexp
+
       digit = value.slice(-2, 2)
       control = ''
-      if value.size == 14
+      if value_only_digits.size == 14
         factor = 0
         2.times do |i|
           sum = 0;
           12.times do |j|
-            sum += value.slice(j, 1).to_i * ((11 + i - j) % 8 + 2)
+            sum += value_only_digits.slice(j, 1).to_i * ((11 + i - j) % 8 + 2)
           end
           sum += factor * 2 if i == 1
           factor = 11 - sum  % 11
--- lib/validates_cpf_cnpj/cpf.rb
+++ lib/validates_cpf_cnpj/cpf.rb
@@ -3,18 +3,18 @@ module ValidatesCpfCnpj
     @@invalid_cpfs = %w{12345678909 11111111111 22222222222 33333333333 44444444444 55555555555 66666666666 77777777777 88888888888 99999999999 00000000000}
 
     def self.valid?(value)
-      value.gsub!(/[^0-9]/, '')
+      value_only_digits = value.gsub(/[^0-9]/, '')
 
-      return false if @@invalid_cpfs.member?(value)
-      
-      digit = value.slice(-2, 2)
+      return false if @@invalid_cpfs.member?(value_only_digits)
+
+      digit = value_only_digits.slice(-2, 2)
       control = ''
-      if value.size == 11
+      if value_only_digits.size == 11
         factor = 0
         2.times do |i|
           sum = 0
           9.times do |j|
-            sum += value.slice(j, 1).to_i * (10 + i - j)
+            sum += value_only_digits.slice(j, 1).to_i * (10 + i - j)
           end
           sum += (factor * 2) if i == 1
           factor = (sum * 10) % 11
